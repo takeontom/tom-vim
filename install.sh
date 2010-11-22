4c2e5d26ad106f9edf46dd26894476588fd11f85
Tom Smith <tom@takeontom.com>
2010-11-22 22:14:22 +0000

    added the installation of ctags to the install script

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 sDir=$(cd $(dirname "$0"); pwd)
 echo "script found at:" $sDir
 
+echo "installing ctags"
+sudo apt-get update
+sudo apt-get install ctags
+
 echo "backing up existing vim config..."
 mv ~/.vimrc ~/.vimrc.backup
 mv ~/.vim ~/.vim.backup
