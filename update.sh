2f07cd327640a80fdb9c68af3b888af0e931a600
Tom Smith <tom@takeontom.com>
2013-02-04 23:13:06 +0000

    ensure vundle install only ran once on first install

--- update.sh
+++ update.sh
@@ -3,8 +3,18 @@ sDir=$(cd $(dirname "$0"); pwd)
 cd $sDir
 
 cd $sDir/vim
-vim +BundleClean! +qall
-vim +BundleInstall! +qall
+
+# vundle install happens by simply loading vim, so only
+# run the update scripts if it's already installed, else
+# will try to do a BundleInstall twice (which is slow)
+if [ -d $sDir/vim/bundle ]
+then
+    vim +BundleClean! +qall
+    vim +BundleInstall! +qall
+else
+    vim +qall
+fi
+
 cd $sDir
 
 cd $sDir/vim/bundle/Command-T/ruby/command-t
