161c5358f16401845276390241abd46c70081536
Tom Smith <tom@fizzynova.com>
2014-05-20 22:30:21 +0100

    improved the installation of ack on non-ubuntu machines

--- post-update.sh
+++ post-update.sh
@@ -2,4 +2,6 @@
 sDir=$(cd $(dirname "$0"); pwd)
 
 mkdir ~/bin
-curl http://beyondgrep.com/ack-2.12-single-file > ~/bin/ack && chmod 0755 ~/bin/ack
+curl http://beyondgrep.com/ack-2.12-single-file > /tmp/ack
+sudo mv /tmp/ack /usr/bin/ack
+sudo chmod 0755 /usr/bin/ack
