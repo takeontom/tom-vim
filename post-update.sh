e50512c0e6fcfce32ab164ca13d668c200b1eb46
Tom Smith <tom@takeontom.com>
2014-05-24 13:35:18 +0100

    install fonts for powerline

--- post-update.sh
+++ post-update.sh
@@ -5,3 +5,9 @@ mkdir ~/bin
 curl http://beyondgrep.com/ack-2.12-single-file > /tmp/ack
 sudo mv /tmp/ack /usr/bin/ack
 sudo chmod 0755 /usr/bin/ack
+
+sudo mkdir -p /usr/share/fonts/opentype
+sudo wget -O /usr/share/fonts/opentype/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf
+sudo fc-cache -f
+sudo wget -O /etc/fonts/conf.d/10-powerline-symbols.conf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+
